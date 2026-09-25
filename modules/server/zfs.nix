b5568287d040e9fb9de401453d34d4199f8e1173
ruben <89784657+RubenBrusselmans@users.noreply.github.com>
2026-09-25 12:45:39 +0200

    fix(server): cap zfs arc at 12g and disable atime on data/crypt

--- modules/server/zfs.nix
+++ modules/server/zfs.nix
@@ -3,6 +3,8 @@
 {
   # ZFS needs the default LTS kernel (override latest)
   boot.kernelPackages = pkgs.linuxPackages;
+  # Cap ARC at 12GiB
+  boot.kernelParams = [ "zfs.zfs_arc_max=12884901888" ];
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs = {
     forceImportRoot = false;
@@ -25,6 +27,19 @@
     autoScrub = {
       enable = true;
       pools = [ "data" ];
+    };
+  };
+
+  # atime=off
+  systemd.services.zfs-atime = {
+    description = "Set atime=off on the data/crypt dataset";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "zfs-import-data.service" ];
+    requires = [ "zfs-import-data.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.zfs}/bin/zfs set atime=off data/crypt";
     };
   };
 }
